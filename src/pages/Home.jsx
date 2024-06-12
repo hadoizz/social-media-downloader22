3ce79cad43a276bd456940d7924fb19406e028ff
hadoizz <82988056+hadoizz@users.noreply.github.com>
2024-06-13 00:36:18 +0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import Layout from '../components/Layout'
-import Form from '../components/Form'
-import axios from 'axios'
+import React, { useState } from 'react';
+import Layout from '../components/Layout';
+import Form from '../components/Form';
+import axios from 'axios';
 
 export default function Home() {
   const [mediaData, setMediaData] = useState(null);
@@ -11,7 +11,7 @@ export default function Home() {
     e.preventDefault();
     const mediaUrl = e.target.mediaUrl.value;
 
-    // verify if the url is empty
+    // Verify if the url is empty
     if (!mediaUrl) {
       alert('Please enter a valid url');
       return;
@@ -41,8 +41,12 @@ export default function Home() {
   return (
     <Layout>
       <div className="text-center py-2 space-y-2 m-2">
-        <h1 className="text-2xl md:text-4xl capitalize text-center text-bold bg-gradient-to-r from-rose-700 to-pink-600 bg-clip-text text-transparent"> Free Downloader from Facebook, Youtube, Instagram, Tiktok . </h1>
-        <p className="text-sm md:text-lg text-center bg-gradient-to-r from-green-200 via-green-400 to-green-500 bg-clip-text text-transparent">Just paste the link and download the video you want.</p>
+        <h1 className="text-2xl md:text-4xl capitalize text-center text-bold bg-gradient-to-r from-rose-700 to-pink-600 bg-clip-text text-transparent">
+          Free Downloader from Facebook, Youtube, Instagram, Tiktok.
+        </h1>
+        <p className="text-sm md:text-lg text-center bg-gradient-to-r from-green-200 via-green-400 to-green-500 bg-clip-text text-transparent">
+          Just paste the link and download the video you want.
+        </p>
         <Form mediaDownload={downloadMedia} />
         
         {loading && (
@@ -52,9 +56,9 @@ export default function Home() {
         )}
         
         <div className="flex justify-center">
-          {mediaData && mediaData.url && (
+          {mediaData && mediaData.medias && (
             <video controls className="w-full md:w-6/12 max-h-80 rounded-md">
-              <source src={mediaData.url} type="video/mp4" />
+              <source src={mediaData.medias[0].url} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           )}
@@ -62,16 +66,20 @@ export default function Home() {
 
         <div className="flex justify-center">
           {mediaData && mediaData.title && (
-            <h2 className="text-2xl md:text-4xl capitalize text-center text-bold bg-gradient-to-r from-rose-700 to-pink-600 bg-clip-text text-transparent">{mediaData.title}</h2>
+            <h2 className="text-2xl md:text-4xl capitalize text-center text-bold bg-gradient-to-r from-rose-700 to-pink-600 bg-clip-text text-transparent">
+              {mediaData.title}
+            </h2>
           )}
         </div>
 
         <div className="flex justify-center space-x-3">
-          {mediaData && mediaData.links && mediaData.links.map((link, index) => (
-            <a key={index} href={link.url} download className="bg-blue-500 text-white p-2 bg-gradient-to-r from-rose-700 to-pink-600">Download {link.quality}</a>
+          {mediaData && mediaData.medias && mediaData.medias.map((media, index) => (
+            <a key={index} href={media.url} download className="bg-blue-500 text-white p-2 bg-gradient-to-r from-rose-700 to-pink-600">
+              Download {media.quality}
+            </a>
           ))}
         </div>
       </div>
     </Layout>
-  )
+  );
 }
