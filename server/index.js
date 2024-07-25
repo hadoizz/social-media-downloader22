383144306ec263a44509e1dec7176c8ee939b0f0
hadoizz <82988056+hadoizz@users.noreply.github.com>
2024-07-25 10:51:13 +0400

    Update and rename index.js to server.js

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const proxy = require('./proxy');
-
-app.use('/proxy', proxy);
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
