45c8a3e92fcfdf521d7110f214d45d53c608718b
hadoizz <82988056+hadoizz@users.noreply.github.com>
2024-07-25 10:08:07 +0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Navber from './Navber'
-import Footer from './Footer'
+import React from 'react';
+import Navber from './Navber';
+import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navber />
-      <main>
+      <main className="flex-grow">
         {children}
       </main>
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
