5105ecf4bae2931d5b396ecc62792e2d713f0354
hadoizz <82988056+hadoizz@users.noreply.github.com>
2024-07-25 10:56:38 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/proxy': {
-        target: 'https://media.storyclone.com', // Your target domain
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/proxy/, ''), // Rewrite URL if needed
-      },
-    },
-  },
-});
+})
