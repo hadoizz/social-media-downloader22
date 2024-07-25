79a3727d3cca34a672cd6743b5b67011ac77045e
hadoizz <82988056+hadoizz@users.noreply.github.com>
2024-07-25 10:38:53 +0400

    Update proxy.js

--- server/proxy.js
+++ server/proxy.js
@@ -1,32 +1,19 @@
-// server/proxy.js
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
+const router = express.Router();
 
-const app = express();
-const PORT = 5000;
-
-app.use(cors());
-
-app.get('/proxy', async (req, res) => {
+router.get('/', async (req, res) => {
   const { url } = req.query;
-  
   if (!url) {
-    return res.status(400).send('URL is required');
+    return res.status(400).send('URL query parameter is required');
   }
 
   try {
-    const response = await axios.get(url, {
-      responseType: 'arraybuffer',
-    });
-
-    res.set('Content-Type', response.headers['content-type']);
-    res.send(response.data);
+    const response = await axios.get(url, { responseType: 'stream' });
+    response.data.pipe(res);
   } catch (error) {
     res.status(500).send('Error fetching the URL');
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Proxy server is running on http://localhost:${PORT}`);
-});
+module.exports = router;
